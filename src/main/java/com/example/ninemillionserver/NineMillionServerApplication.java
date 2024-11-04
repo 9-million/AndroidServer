9981bd120b19bc6a45311cd90bd9bb5858c46ee5
user <wkd5878@gmail.com>
2024-11-04 09:56:08 +0900

    :tada: init

---
+++ src/main/java/com/example/ninemillionserver/NineMillionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ninemillionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineMillionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NineMillionServerApplication.class, args);
+    }
+
+}
